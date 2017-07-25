860817d982213b4f86cb8cf5add31e5c05fc38a7
Mac User <sabbey37@gmail.com>
2017-07-25 14:47:40 -0400

    Print title to console

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 var movieURL = 'http://www.omdbapi.com/?apikey=1ca32dee&';
 var posterURL = 'http://img.omdbapi.com/?apikey=1ca32dee&';
 var $searchField = $('[data-role="search-form"]');
+var $movieDisplay = $('[data-role="display-movies"]');
 
 function getMovieData(search) {
     var req = $.get(movieURL + "s=" + search);
@@ -8,6 +9,11 @@ function getMovieData(search) {
         .then(makeMovieElement);
 } 
     
+function getPosterData(ident) {
+    var req = $.get(posterURL + "h=250&" + ident);
+    req
+} 
+
 function listMovies(orders) {
     localStorage.setItem('priorOrders', JSON.stringify(orders));
 }
@@ -25,12 +31,28 @@ function addMovieSearchListner() {
         var searchValue = $('[data-role="movie-search"]').val();
         console.log(searchValue);
         getMovieData(searchValue);
-
     })
 }
 
 function makeMovieElement(response) {
-    console.log(response);
+    var responseArray = response["Search"];
+    var $searchResults = $('<div></div>', {
+        'class': 'results',
+        'data-draw': 'results'
+    });
+    responseArray.forEach(function(movie, i) {
+        var $movieDiv = $('<div></div>', {
+        'class': 'movie',
+        'data-draw': 'movie'
+        });
+        var $title = $('<span></span>', {
+            'text': responseArray[i]['Title'] + " (" + responseArray[i]['Year'] + ")"
+        })
+        $movieDiv.append($title);
+        $searchResults.append($movieDiv);
+    })
+
+    $movieDisplay.append($searchResults);
 }
 
 function makePosterElement() {
