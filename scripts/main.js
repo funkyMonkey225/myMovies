e71da09b35a2145b4400ac727d36a7147aeb5f43
Mac User <sabbey37@gmail.com>
2017-07-25 14:16:58 -0400

    Add movie search request to API upon submit

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,44 @@
+var movieURL = 'http://www.omdbapi.com/?apikey=1ca32dee&';
+var posterURL = 'http://img.omdbapi.com/?apikey=1ca32dee&';
+var $searchField = $('[data-role="search-form"]');
 
+function getMovieData(search) {
+    var req = $.get(movieURL + "s=" + search);
+    req
+        .then(makeMovieElement);
+} 
+    
+function listMovies(orders) {
+    localStorage.setItem('priorOrders', JSON.stringify(orders));
+}
 
+function deleteMovie(movie) {
+    var req = $.ajax ({
+        url: URL + '/' + email.attr('name'),
+        method: "DELETE"
+    })
+}
 
+function addMovieSearchListner() {
+    $searchField.submit(function (event){
+        event.preventDefault();
+        var searchValue = $('[data-role="movie-search"]').val();
+        console.log(searchValue);
+        getMovieData(searchValue);
 
-function makeMovieElement() {
+    })
+}
+
+function makeMovieElement(response) {
+    console.log(response);
+}
+
+function makePosterElement() {
 
 }
+
+function main() {
+    addMovieSearchListner();
+}
+
+main();
